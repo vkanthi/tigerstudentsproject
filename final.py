1800ebe5759a3d153228d51ef804c24fe34ec55a
Malakanthi6 <malaeswar01@gmail.com>
2021-01-24 00:52:30 +0530

    Tictactoe file with 3 functions

--- final.py
+++ final.py
@@ -1,1 +1,15 @@
 # This is tic tac toe project
+
+
+
+
+
+
+
+
+
+def place_player_symbol(board, position, player_symbol):
+	"""This function place the player symbol on the desired position"""
+	board[position] = symbol
+
+

ca0728e55b7b6517831776c5e36212f0b045eed0
malaeswar <malaeswar01@gmail.com>
2021-01-27 19:05:22 +0530

    Added few more words

--- mala.py
+++ mala.py
@@ -1,7 +1,8 @@
-#This is mala file 
-#Second line
-#Third line added to check git config 
-#4th line
-#This is mala editing the 5th line in mala's file
-#This is mala editing the 6th line in mala's file on 25012021 
-#7th line of the file needs to be added   
+# This is mala file mala
+# Second line mala   
+# Third line added to check git config  mala viji
+# 4th line mala viji
+# This is mala editing the 5th line in mala's file mala viji
+# This is mala editing the 6th line in mala's file on 25012021 mala viji
+# 7th line of the file needs to be added mala viji
+# 8th line added mala viji

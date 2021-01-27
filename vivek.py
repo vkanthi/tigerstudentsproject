cec75e19bc2f823e64a64f1b510649b0597b6c2a
vkanthi <vkanthi@hotmail.com>
2021-01-27 12:51:20 -0500

    Delete vivek.py

--- vivek.py
+++
@@ -1,8 +0,0 @@
-# This is my sample file new
-# This is the neew line. new
-# This is line 2 new
-# This is line 3 new
-# This is line 4 new
-# This is the new line number 5
-# Here is the new line. 
-# This file is again edited by vivek on 27th jan 2021

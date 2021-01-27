c355b53ba75e7712f8974233131e07460da20fb8
vkanthi <vkanthi@hotmail.com>
2021-01-27 12:50:46 -0500

    Delete mala.py

--- mala.py
+++
@@ -1,8 +0,0 @@
-# This is mala file mala
-# Second line mala   
-# Third line added to check git config  mala viji
-# 4th line mala viji
-# This is mala editing the 5th line in mala's file mala viji
-# This is mala editing the 6th line in mala's file on 25012021 mala viji
-# 7th line of the file needs to be added mala viji
-# 8th line added mala viji

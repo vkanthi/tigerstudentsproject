103137a2b515b1b77cbab324f01edd185dee9529
Viji-6041 <vijiv6041@gmail.com>
2021-01-28 00:55:31 +0530

    Created a program file

---
+++ calculation_palindrome.py
@@ -0,0 +1,33 @@
+# Creating a function to collect information from the user
+
+
+def user_name():
+    """This function is to ask the user name"""
+    your_name = str(input("Enter your name here:- ")).capitalize()
+    return your_name
+
+
+def user_input():
+    """This function is to collect the user input to play the palindrome """
+    your_input = input("Enter your input as a palindrome word or number")
+    return your_input
+
+
+def outcome():
+    """ """
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

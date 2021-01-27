88688a72aaeb1946a16a85b438ea58ce0728ee5c
malaeswar <malaeswar01@gmail.com>
2021-01-28 00:02:08 +0530

    Created user_name and user_input function for PLAINDROME project

---
+++ getting_username.py
@@ -0,0 +1,22 @@
+# Getting user name from the user 
+
+
+def user_name():
+    """Getting name from the user"""
+    user__name = str(input("Enter your name:- ")).capitalize()
+
+
+username()
+
+
+# Getting input for the palindrome function from the user
+
+
+def user_input():
+    """Getting user input for the palindrome function"""
+    user__input = str(input("Enter a input you want the palindrome function to be performed:- ")).lower()
+
+
+user_input()
+
+

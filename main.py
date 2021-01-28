630fb8e6201511fb8eb35dd479358bc2eb877cb7
malaeswar <malaeswar01@gmail.com>
2021-01-28 21:26:47 +0530

    Edited getting user name file

---
+++ main.py
@@ -0,0 +1,3 @@
+from modules.getting_username import user_name
+
+print("This is " + user_name())
--- modules/getting_username.py
+++ modules/getting_username.py
@@ -3,11 +3,8 @@
 
 def user_name():
     """Getting name from the user"""
-    user__name = str(input("Enter your name:- ")).capitalize()
-    return user__name
-
-
-username()
+    name = str(input("Enter your name:- ")).capitalize()
+    return name
 
 
 # Getting input for the palindrome function from the user
@@ -15,10 +12,7 @@ username()
 
 def user_input():
     """Getting user input for the palindrome function"""
-    user__input = str(input("Enter a input you want the palindrome function to be performed:- ")).lower()
-    return user__input
-
-
-user_input()
+    user_word = str(input("Enter a input you want the palindrome function to be performed:- ")).lower()
+    return user_word
 
 

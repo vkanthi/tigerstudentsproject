5efba44d0cb191d2a20a9a83a93d013e7959b865
Viji-6041 <vijiv6041@gmail.com>
2021-01-28 23:33:59 +0530

    Modified calculation_palindrome.py

--- modules/calculation_palindrome.py
+++ modules/calculation_palindrome.py
@@ -1,41 +1,8 @@
-# Creating a function to collect information from the user
+# This is a calculation function
 
 
-def user_name():
-    """This function is to ask the user name"""
-    your_name = str(input("Enter your name here:- ")).capitalize()
-    return your_name
-
-
-user_name()
-
-
-def user_input():
-    """This function is to collect the user input to play the palindrome """
-    your_input = input("Enter your input as a palindrome word or number")
-    return your_input
-
-
-user_input()
-
-
-def outcome():
+def calculation():
     """ """
     pass
-
-
-outcome()
-
-
-
-
-
-
-
-
-
-
-
-
 
 

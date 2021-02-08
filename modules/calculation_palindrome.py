933a3a22fa70d8a7a6e186cea8ea1d8dc849fe61
vkanthi <vkanthi@hotmail.com>
2021-02-08 11:42:36 -0500

    Delete modules directory

--- modules/calculation_palindrome.py
+++
@@ -1,9 +0,0 @@
-# This is a calculation function
-
-
-def palindrome(user_word):
-    """This function reverses the user's word which was given as an input  """
-    result = user_word[::-1]
-    return result
-
-

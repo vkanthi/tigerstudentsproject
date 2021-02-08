47b6bd5bac0d28eb50b3b96592e173610296e07b
malaeswar <malaeswar01@gmail.com>
2021-02-08 22:29:39 +0530

    Getting user name and user input file

---
+++ modules/getting_username.py
@@ -0,0 +1,18 @@
+# Getting user name from the user 
+
+
+def user_name():
+    """Getting name from the user"""
+    name = str(input("Enter your name:- ")).capitalize()
+    return name
+
+
+# Getting input for the palindrome function from the user
+
+
+def user_input():
+    """Getting user input for the palindrome function"""
+    user_word = str(input("Enter a input you want the palindrome function to be performed:- ")).lower()
+    return user_word
+
+
